853f858f9d284ccb5102e6057e42d70d1aa8c416
Matthew Ruston <artanis0@gmail.com>
2018-12-11 19:19:06 -0500

    day 6 part 1 solution

--- day_6_chronal_coordinates.py
+++ day_6_chronal_coordinates.py
@@ -1,7 +1,7 @@
 import numpy
 import math
 
-words = open('inputs\\day_6.txt', 'r')
+words = open('inputs/day_6.txt', 'r')
 lines = words.read().splitlines()       # Prevents trailing newline characters
 input = []
 for l in lines:
@@ -9,33 +9,47 @@ for l in lines:
     input.append((int(parsed[0]), int(parsed[1])))
 
 # test input
-input = [(1, 2), (1, 6), (8, 3), (3, 4), (5, 5), (8, 9)]
+#input = [(1, 1), (1, 6), (8, 3), (3, 4), (5, 5), (8, 9)]
+
+# Used for displaying test input
+owners_to_alpha = {
+        "0": "a",
+        "1": "b",
+        "2": "c",
+        "3": "d",
+        "4": "e",
+        "5": "f"
+    }
 
 def manhattan_distance(x1, y1, x2, y2):
     return abs(x1 - x2) + abs(y1 - y2)
 
 def calculate_owner(x, y, data):
+    # owner is the coordinate that has the closest manhattan distance to the given (x, y) coordinate.
     min_distance = 99999
     owner = None
     for index, coordinate in enumerate(data):
         distance = manhattan_distance(coordinate[0], coordinate[1], x, y)
-        print(f"Distance between {x}, {y} and {coordinate[0]},{coordinate[1]} is {distance}")
         if distance < min_distance:
             min_distance = distance
             owner = index
         elif distance == min_distance:
-            owner = '.'     # dot means conflict.min_distance
+            owner = '.'     # return a period if this coordinate has > 1 possible owner.
         if distance == 0:
-            owner = "*"
-    print(f"Position {x},{y} is owned by {owner}")
+            owner = index
     return owner
+
+def print_map(input, size_x, size_y):
+    print("")
+    for y in range(size_y):
+        print('')
+        for x in range(size_x):
+            print(owner_map[x][y], end='')
+    print('')
     
 
-#plane_size_x = sorted(input, key=lambda x: x[0])[-1][0]
-#plane_size_y = sorted(input, key=lambda x: x[1])[-1][1]
-
-plane_size_x = 10
-plane_size_y = 10
+plane_size_x = sorted(input, key=lambda x: x[0])[-1][0] + 1
+plane_size_y = sorted(input, key=lambda x: x[1])[-1][1] + 1
 
 owner_map = []
 
@@ -47,28 +61,35 @@ for x in range(plane_size_x):
         if owner_index != None:
             owner_map[x][y] = owner_index
 
-print('\n'.join([''.join(['{:4}'.format(item) for item in row]) 
-      for row in owner_map]))
+print_map(owner_map, plane_size_x, plane_size_y)
 
-# ## We need to remove owners who are infinite, so go around the outside edge of hte plane
-# for x in range(plane_size_x):
-#     for y in range(plane_size_y):
-#         if x == 0 or x == plane_size_x -1 or y == 0 or y == plane_size_y -1:
-#             if owner_map[x, y] != None:
-#                 input[int(owner_map[x, y])] = None
-# print("Remaining...")
-# print(input)
+# Remove coordinates whose ownership could be infinite.
+for x in range(plane_size_x):
+    for y in range(plane_size_y):
+        if x == 0 or x == plane_size_x -1 or y == 0 or y == plane_size_y -1:
+            if owner_map[x][y] != ".":
+                input[int(owner_map[x][y])] = None
 
-# sizes = {}
-# # Get the size
-# for x in range(plane_size_x):
-#     for y in range(plane_size_y):
-#         owner = owner_map[x,y]
-#         if owner != None:
-#             if str(owner) not in sizes:
-#                 sizes[str(owner)] = 0
-#             sizes[str(owner)] += 1
-# print(sizes)
-        
+print("Remaining coordinates...")
+print(input)
+
+sizes = {}
+# Get the size
+for x in range(plane_size_x):
+    for y in range(plane_size_y):
+        owner = owner_map[x][y]
+        if owner != None:
+            if str(owner) not in sizes:
+                sizes[str(owner)] = 0
+            sizes[str(owner)] += 1
+print(sizes)
+
+max_size = 0
+for index, val in enumerate(input):
+    if val != None:
+        print(f"Size for {val} is {sizes[str(index)]}")
+        if sizes[str(index)] > max_size:
+            max_size = sizes[str(index)]
+print(f"Part One: {max_size} is max size")
     
 
